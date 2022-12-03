52f541d4d0c10c318fd538e7d4c85487e1418955
Артур Алтынчурин <alturik9@yandex.ru>
2022-12-03 21:54:36 +0500

    home_work1_2

---
+++ 1_lesson/HOME_WORK_1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает
+// число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+Console.WriteLine("Enter number");
+string N = Console.ReadLine();
+int A = int.Parse(N);
+
+
+if (A % 2 == 0)
+{
+    Console.WriteLine("even number");
+}
+else
+{
+    Console.WriteLine("odd number enter the following");
+}
