b6814e24b2ce4c614f884930117ebfd93e5f1147
Артур Алтынчурин <alturik9@yandex.ru>
2022-12-03 19:39:58 +0500

    home_work1_0

---
+++ 1_lesson/HOME_WORK_1_0/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("enter number 1");
+string N1 = Console.ReadLine();
+int a = int.Parse(N1);
+
+Console.WriteLine("enter number 2");
+string N2 = Console.ReadLine();
+int b = int.Parse(N2);
+
+if (a > b)
+{
+    Console.WriteLine($"The number {a} is greater than {b}");
+}
+
+else if (a == b)
+{
+    Console.Write($"The number {a} is equal to the number {b}");
+
+}
+else
+{
+    Console.Write($"The number {b} is greater than {a}");
+}
