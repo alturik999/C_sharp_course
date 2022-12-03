3233a416f61582dbca3260c8ec56cb44b2102c83
Артур Алтынчурин <alturik9@yandex.ru>
2022-12-03 22:43:06 +0500

    home_work1_3

---
+++ 1_lesson/HOME_WORK_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter number");
+string Num = Console.ReadLine();
+int N = int.Parse(Num);
+int a = 2;
+
+while (a <= N)
+{
+    Console.Write($"{a} ");
+    a += 2;
+}
