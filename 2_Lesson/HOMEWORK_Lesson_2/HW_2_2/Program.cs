48508574682a4407eea47c2e78c8b39a03c8c4be
Артур Алтынчурин <alturik9@yandex.ru>
2022-12-04 21:36:22 +0500

    Homework2_2

---
+++ 2_Lesson/HOMEWORK_Lesson_2/HW_2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет. 
+
+string Num (int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+        return $"{num} -> {num % 10}";
+    return "The number is not three-digit!";
+}
+Console.WriteLine(Num(int.Parse(Console.ReadLine())));
