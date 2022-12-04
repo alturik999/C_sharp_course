d88a19c0bea4c395b7b91090e23033dea74d97c4
Артур Алтынчурин <alturik9@yandex.ru>
2022-12-04 21:00:20 +0500

    Homework2_0

---
+++ 2_Lesson/HOMEWORK_2_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int Number(int num)
+{
+    Console.WriteLine(num);
+    return (num / 10 % 10);
+}
+
+Console.WriteLine (Number(new Random().Next(100, 1000)));
