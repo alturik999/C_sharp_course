1a745a5a5734f0e37860b4358383cced97118455
Артур Алтынчурин <alturik9@yandex.ru>
2022-12-12 21:36:19 +0500

    4_3

--- 4_Lesson/4_3/Program.cs
+++ 4_Lesson/4_3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая принимает на вход
+//    число N и выдаёт произведение чисел от 1 до N.
+
+int PowNum(int num)
+{
+    int all_mul = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_mul *= i;
+    }
+    return all_mul;
+}
+
+string answer = Console.ReadLine();
+int ans = answer == null ? 0 : int.Parse(answer);
+Console.WriteLine(PowNum(ans));
